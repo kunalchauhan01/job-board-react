34dd263bd75eca9143f6ee6a53dac1f9a479e6be
kunalchauhan01 <kunalchauhan6767@gmail.com>
2025-05-20 21:59:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbar.jsx'
+
+function App() {
+  return (
+   
+    <div>
+      <Navbar />
+      <h1>Welcome to the Job Board!</h1>
+      <p>This is the beginning of your React journey.</p>
+    </div>
+  );
+}
+
+export default App;
+
